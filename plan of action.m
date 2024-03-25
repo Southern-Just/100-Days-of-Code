3f6e4b3d6baf05e630a5dfdf05c86e65c3b8ad7e
Southern Just <117814631+Southern-Just@users.noreply.github.com>
2024-03-25 12:09:10 +0300

    plan of action.md

---
+++ plan of action.m
@@ -0,0 +1,71 @@
+Phase 1: Fundamentals (Days 1-15) ♨️🚀  
+
+
+📌Days 1-5: Introduction to C++
+
+Objective: Gain familiarity with basic C++ syntax and structure.
+Topics: Variables, Data Types, Input/output Operations.
+Activities: Reading tutorials, practicing simple programs.
+
+📌Days 6-10: Control Flow and Functions
+
+Objective: Understand control structures and function usage.
+Topics: Loops, Conditional Statements, Function Definition.
+Activities: Writing decision-making programs, practicing loops.
+
+📌Days 11-15: Arrays, Strings, and Pointers
+
+Objective: Learn about arrays, strings, and pointers in C++.
+Topics Covered: Array manipulation, String handling, Basic Pointer Operations.
+Activities: Completing exercises involving arrays, strings, and pointers.
+
+Phase 2: Mini Projects (Days 16-45)
+
+📌Days 16-25: Console-based Mini Projects
+
+Objective: Reinforce C++ fundamentals through small projects.
+Examples: Tic Tac Toe game, Calculator application, Text-based utilities.
+Activities: Developing simple console-based applications.
+
+Days 26-35: Data Structures and Algorithms
+
+Objective: Understand fundamental data structures and algorithms.
+Topics Covered: Arrays, Linked Lists, Sorting Algorithms.
+Activities: Implementing basic data structures and algorithms.
+
+📌Days 36-45: Object-Oriented Programming (OOP)
+
+Objective: Dive into OOP principles and practice.
+Topics Covered: Classes, Objects, Inheritance, Polymorphism.
+Activities: Creating classes, building projects using OOP concepts.
+
+Phase 3: Project Development (Days 46-75)
+
+📌Days 46-60: Project Planning and Design
+Objective: Plan and design a C++ project aligned with learning goals.
+Activities: Choosing project ideas, designing project structure.
+
+📌Days 61-75: Project Implementation
+
+Objective: Implement the planned project and test regularly.
+Activities: Breaking down development tasks, coding, testing, documenting.
+
+Phase 4: Skill Polishing (Days 76-100)
+
+📌Days 76-85: Code Review and Optimization
+
+Objective: Review project code, optimize for performance.
+Activities: Refactoring, optimizing code, learning best practices.
+
+📌Days 86-95: Advanced Topics and Libraries
+
+Objective: Explore advanced C++ topics and libraries.
+Topics Covered: Templates, Exceptions, STL, Boost.
+Activities: Experimenting with advanced concepts and libraries.
+
+📌Days 96-100: Project Enhancement and Conclusion
+
+Objective: Enhance project, gather feedback, reflect on learning journey.
+Activities: Adding features to my project, seeking feedback, reflection.
+		Sicut me Deus
+
